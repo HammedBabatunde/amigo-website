04c40b1ad515b6e22be092a9caf99eb39c11381f
Ezihe Godswill <ezihegodswill01@gmail.com>
2021-10-29 03:19:35 +0100

    added gsap js, page content in index.html

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+const tl = gsap.timeline();
+
+window.addEventListener('load', () => {
+  tl.from('.home-text-ctn h1 span', {
+    y: "100",
+    duration: 1
+  })
+  tl.from('.home-text-ctn p', {
+    y: "20px",
+    opacity: 0
+  })
+})
