14cf4230d30bfcaec6d21896fcb27aa35b18bca8
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-10-30 10:03:12 +0100

    lint files with vscode's prettier

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const tl = gsap.timeline({ default: { duration: 0.6 } });
 
 window.addEventListener('load', () => {
   tl.from('.home-text-ctn h1 span', {
-    y: "100%",
+    y: '100%',
     opacity: 0,
     stagger: 0.25
-  })
+  });
   tl.from('.home-text-ctn p', {
     opacity: 0
-  })
-})
+  });
+});
